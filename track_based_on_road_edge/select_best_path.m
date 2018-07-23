c3be73bc5f59dfe07b4ab434ff8e34ac0943a6a3
tradye <tradye@163.com>
2018-07-23 10:29:22 +0800

    LKA based on road edge

---
+++ track_based_on_road_edge/select_best_path.m
@@ -0,0 +1,18 @@
+[cpy,ky]=select_best_path(x,x_max,kr_max,kl_max,x0,y0,fl,fr);%对控制空间内的每条路径进行离散化
+%输入：左右两侧道路的曲率、坐标，初始位置，
+%
+delta_k = 0.01;% delta_k是一个重要的参数,离散的步长
+w1 = 0.5;
+w2 = 0.5;
+p = floor((kl_max - kr_max) / delta_k);
+%% 路径离散化
+for j = 1:1:(p + 1)
+   kp(j) = kr_max + (kl_max - kr_max) / delta_k * (j - 1); 
+   sj(j) = asin(kr);
+   fj(j) = 
+   C_proximity = min(fabs(),fabs());
+end
+%xr0=linspace(0,x_max,100);
+%sr0=asin(kr(i)*(xr0-x0))/kr(i);
+%yr0=-1/kr(i)*cos(kr(i)*sr0)+1/kr(i)+y0;
+
