8ad08a8b424d8941cba30f9a80fa4f68c3f7449e
tradye <tradye@163.com>
2018-07-24 11:53:58 +0800

    the inital k0 is very important.change the computing way of C_proximity and C_smooth

--- track_based_on_road_edge/road_edge.m
+++ track_based_on_road_edge/road_edge.m
@@ -1,5 +1,5 @@
 %% 基于道路边沿采样的车道保持算法
-alfa = 0; % 30 -30 45 -45 表示车辆航向角和道路的夹角
+alfa = 45; % 30 -30 45 -45 表示车辆航向角和道路的夹角
 %初始化
 if alfa == 0
     for i=1:100
@@ -49,13 +49,13 @@ plot(x,fr);
 kmax=1.5;klmin=kmax;krmax=-kmax;
 x0=0;y0=0;s_thresh=1.2;
 pi = 3.1415926;
-K_expect = 0.0;
-w1 = 1.0;
+K_expect = 0.08;
+w1 = 0.2;
 w2 = 0.1;
 C_cost_min = 0.0;
 
 delta_k = 0.05;
-C_p = 0.0;
+C_p = 0.000001;
 C_s = 0.0;
 %求解一次圆弧轨迹
 for i=1:100
@@ -89,44 +89,44 @@ for i=1:100
         break;
     else
         
-        p = floor((klmin - krmax) / delta_k);
-        p = 100;
-        C_p = 0.000001;
-        C_s = 0.0;
-        for j = 1:1:(p )
-%            kp(j) = krmax + delta_k * (j - 1); 
-            kp = linspace(krmax,klmin,p);
-            xp_ = linspace(0,x_max,p);
-            sp_ = asin(kp(j)*(xp_-x0))/kp(j);
-            yp_ = -1/kp(j)*cos(kp(j)*sp_)+1/kp(j)+y0;
-           
-%             tmp = min(abs(yp_(i) - fl(i)),abs(yp_(i) - fr(i)));
+%         p = floor((klmin - krmax) / delta_k);
+%         p = 100;
+%         C_p = 0.000001;
+%         C_s = 0.0;
+%         for j = 1:1:(p )
+% %            kp(j) = krmax + delta_k * (j - 1); 
+%             kp = linspace(krmax,klmin,p);
+%             xp_ = linspace(0,x_max,p);
+%             sp_ = asin(kp(j)*(xp_-x0))/kp(j);
+%             yp_ = -1/kp(j)*cos(kp(j)*sp_)+1/kp(j)+y0;
+%            
+% %             tmp = min(abs(yp_(i) - fl(i)),abs(yp_(i) - fr(i)));
+% %             tmp = min(abs(yp_(j) - fl(i)),abs(yp_(j) - fr(i)));
 %             tmp = min(abs(yp_(j) - fl(i)),abs(yp_(j) - fr(i)));
-            tmp = min(abs(yp_(i) - fl(i)),abs(yp_(i) - fr(i)));
-
-            C_p = C_p + tmp;
-
-            C_proximity(j) = 1 / C_p;
-            C_s = C_s + (kp(j) - K_expect)^2;
-            C_smooth(j) = C_s;
-           
-%            plot(xp_,yp_,'y*');%离散化后的控制空间
-
-        end
-           
-        C_cost = w1* C_proximity + w2 * C_smooth;
-
-        C_cost_min = C_cost(1);
-        for m = 1:1:length(C_cost)
-            if C_cost(m) <= C_cost_min
-                C_cost_min = C_cost(m);
-                K_expect = kp(m);
-                index = m;
-            end 
-        end
-
-        fprintf('K_expect = %f \n',K_expect);
-        fprintf('index = %f \n',index);
+% 
+%             C_p = C_p + tmp;
+% 
+%             C_proximity(j) = 1 / C_p;
+%             C_s = C_s + (kp(j) - K_expect)^2;
+%             C_smooth(j) = C_s;
+%            
+% %            plot(xp_,yp_,'y*');%离散化后的控制空间
+% 
+%         end
+%            
+%         C_cost = w1* C_proximity + w2 * C_smooth;
+% 
+%         C_cost_min = C_cost(1);
+%         for m = 1:1:length(C_cost)
+%             if C_cost(m) <= C_cost_min
+%                 C_cost_min = C_cost(m);
+%                 K_expect = kp(m);
+%                 index = m;
+%             end 
+%         end
+% 
+%         fprintf('K_expect = %f \n',K_expect);
+%         fprintf('index = %f \n',index);
 
     end
     %% 控制空间内道路右侧所有的采样圆弧
@@ -140,16 +140,7 @@ for i=1:100
     yl0=-1/kl(i)*cos(kl(i)*sl0)+1/kl(i)+y0;%根据曲率和弧长，求y方向坐标
     plot(xl0,yl0,'go');%道路左侧所有的采样圆弧
     
-%     if index == 1
-%         xy = linspace(0,x(index),100);
-%     else
-%         xy=linspace(x(index - 1),x(index),100);
-%     end
-%     
-%     sy=asin(K_expect*(xy-x0))/K_expect;
-%     fy=-1/K_expect*cos(K_expect*sy)+1/K_expect+y0;
-%     plot(xy,fy,'r*')
-%     hold on
+
 end
 
 %% 将最优路用红色星型曲线表示出来
@@ -157,10 +148,44 @@ end
 % sy=asin(KY*(xy-x0))/KY;
 %  fy=-1/KY*cos(KY*sy)+1/KY+y0;
 % plot(xy,fy,'r*')
+q = floor((klmin - krmax) / delta_k);
+p = 100;
+
+kp = linspace(krmax,klmin,p);
+
+for j = 1:1:(p )
+    xp_ = linspace(0,x_max,p);
+    sp_ = asin(kp(j)*(xp_-x0))/kp(j);
+    yp_ = -1/kp(j)*cos(kp(j)*sp_)+1/kp(j)+y0;
+
+    for n = 1:1:length(x)
+        tmp = min(abs(yp_( n ) - fl(n)),abs(yp_(n ) - fr(n)));
+        C_p = C_p + tmp;
+        fprintf('C_p = %f \n',C_p);
+    end
+
+    C_proximity(j) = 1 / C_p;
+    C_smooth(j) = (kp(j) - K_expect)^2;
+    C_p = 0.000001;           
+%             plot(xp_,yp_,'y*');%离散化后的控制空间
+
+end
+
+C_cost = w1* C_proximity + w2 * C_smooth;
+C_cost_min = C_cost(1);
+for m = 1:1:length(C_cost)
+    if C_cost(m) <= C_cost_min
+        C_cost_min = C_cost(m);
+        K_expect = kp(m);
+        index = m;
+    end 
+end
+
+fprintf('K_expect = %f \n',K_expect);
+fprintf('index = %f \n',index);  
 
 
-
- xy=linspace(0,x(i),100);
+ xy=linspace(0,x_max,100);
  sy=asin(K_expect*(xy-x0))/K_expect;
  fy=-1/K_expect*cos(K_expect*sy)+1/K_expect+y0;
  plot(xy,fy,'r*')
