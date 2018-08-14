4c1ac83bafe0fbce7fda9b49f35e3709896c03b0
tradye <tradye@163.com>
2018-08-14 11:26:50 +0800

    delete useless files and add matlab linspace function

--- track_based_on_road_edge/select_best_path.m
+++ track_based_on_road_edge/select_best_path.m
@@ -15,4 +15,16 @@ end
 %xr0=linspace(0,x_max,100);
 %sr0=asin(kr(i)*(xr0-x0))/kr(i);
 %yr0=-1/kr(i)*cos(kr(i)*sr0)+1/kr(i)+y0;
+% void linspace(float x0,float xf, float lx[], int sample_num)
+% {
+%     int i;
+%     lx[0] = x0;
+%     lx[sample_num - 1] = xf;
+% 
+%     for (i = 1; i < sample_num - 1; i++)
+%     {
+%         lx[i] = lx[i - 1] + ((xf - x0) / (sample_num - 1));
+%     }
+%     return;
+% }
 
